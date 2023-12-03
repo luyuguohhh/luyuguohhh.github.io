7a947cfcac3bb340f8f145dda3565fcd8f353ad6
luyuguohhh <luyuguohhh@qq.com>
2023-12-03 15:09:46 +0800

    Site updated: 2023-12-03 15:09:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/02/linux基础命令/"];function toRandomPost(){
+var posts=["2023/12/02/linux基础命令/","2023/12/02/Typora配置手册/","2023/12/01/flash钓⻥/","2023/11/28/python环境配置/","2023/12/01/利用github搭建hexo博客/","2023/11/30/vscode/","2023/11/29/钓鱼数据库/","2023/12/01/大灰狼远程控制/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
